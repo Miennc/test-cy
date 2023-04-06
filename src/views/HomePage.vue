a8f7f19b27bdd1c6e0700abdf7235a398d00ca43
mien <2502mien@gmail.com>
2023-04-06 16:12:59 +0700

    add start view

--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -8,6 +8,8 @@
         <div>
           <h2 class="text-[#1e1666] text-[2.375rem] font-[800]  leading-[2.813rem]">Exclusive Technology For
             Provide IT <span class="text-[#2513d1]">Solution</span></h2>
+          <div class="bg-[#2513d1] mt-[0.5rem] w-[10%] h-[4px]">
+          </div>
           <p class="text-[#6a7c92] mt-[2rem] text-[0.938rem] leading-[1.75rem]">
             IT-Solve is the partner of choice for many of the world’s leading and solving enterprises, SMEs and
             technology
@@ -49,6 +51,11 @@
           <!--         <div class="w-[52px] h-[4px] border-4 border-[transparent]">-->
           <!--         </div>-->
         </div>
+
+        <div class="flex justify-center">
+          <div class="bg-[#2513d1] mb-[1rem] w-[10%] h-[4px]">
+          </div>
+        </div>
         <div class="text-center text-[#6a7c92]    text-[0.938rem] leading-[1.75rem]">
           <div class="px-[20rem]">
             Business solution company sit our any how site used the our company any site us it-solve theme is very
@@ -62,31 +69,18 @@
 
       <div class="grid pb-[3rem] grid-cols-3  gap-5">
         <div class="bg-[#FFFFFF] col-span-1 ">
-          <div class="mx-[40px] my-[2rem]">
-            <div class="text-center">
-              <img src="" alt="">
-              icon
-            </div>
-            <div class="text-[#1e1666] mt-[1rem] text-[24px] font-[700] text-center leading-[31px] ">
-              Interface Design
-            </div>
-            <div class="text-[#6a7c92] my-[1rem] text-center font-[15px] leading-[28px] ">
-              IT Solve professional business theme for your company, support quality all time, any help please late me
-              know.
-            </div>
-            <div class="text-center cursor-pointer">
-              <span class="text-[#2513d1]">Discover Now</span> <i class="fa-solid text-[#2513d1] fa-arrow-right"></i>
-            </div>
-          </div>
-        </div>
+          <div class="mx-[20px] my-[2rem]">
+            <div class="flex justify-center ">
+              <div class="w-[120px] h-[120px] relative">
+                <img src="https://wp.dreamitsolution.net/itsolve/wp-content/themes/itsolve/assets/images/service4.png"
+                     alt="">
+                <div class="absolute top-[20%] right-[40%]">
+                  <i class="fa-solid fa-globe text-[#FFFFFF] text-5xl"></i>
+                </div>
+              </div>
 
-        <div class="bg-[#FFFFFF] col-span-1 ">
-          <div class="mx-[40px] my-[2rem]">
-            <div class="text-center">
-              <img src="" alt="">
-              icon
             </div>
-            <div class="text-[#1e1666] mt-[1rem] text-[24px] font-[700] text-center leading-[31px] ">
+            <div class="text-[#1e1666]  text-[24px] font-[700] text-center leading-[31px] ">
               Interface Design
             </div>
             <div class="text-[#6a7c92] my-[1rem] text-center font-[15px] leading-[28px] ">
@@ -94,19 +88,30 @@
               know.
             </div>
             <div class="text-center cursor-pointer">
-              <span class="text-[#2513d1]">Discover Now</span> <i class="fa-solid text-[#2513d1] fa-arrow-right"></i>
+              <a class=" text-indigo-500 inline-flex items-center">Discover Now
+                <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                     class="w-4 h-4 ml-2" viewBox="0 0 24 24">
+                  <path d="M5 12h14M12 5l7 7-7 7"></path>
+                </svg>
+              </a>
             </div>
           </div>
         </div>
 
 
         <div class="bg-[#FFFFFF] col-span-1 ">
-          <div class="mx-[40px] my-[2rem]">
-            <div class="text-center">
-              <img src="" alt="">
-              icon
+          <div class="mx-[20px] my-[2rem]">
+            <div class="flex justify-center ">
+              <div class="w-[120px] h-[120px] relative">
+                <img src="	https://wp.dreamitsolution.net/itsolve/wp-content/themes/itsolve/assets/images/service2.png"
+                     alt="">
+                <div class="absolute top-[20%] right-[35%]">
+                  <i class="fas fa-sitemap text-[#FFFFFF] text-5xl"></i>
+                </div>
+              </div>
+
             </div>
-            <div class="text-[#1e1666] mt-[1rem] text-[24px] font-[700] text-center leading-[31px] ">
+            <div class="text-[#1e1666]  text-[24px] font-[700] text-center leading-[31px] ">
               Interface Design
             </div>
             <div class="text-[#6a7c92] my-[1rem] text-center font-[15px] leading-[28px] ">
@@ -114,18 +119,31 @@
               know.
             </div>
             <div class="text-center cursor-pointer">
-              <span class="text-[#2513d1]">Discover Now</span> <i class="fa-solid text-[#2513d1] fa-arrow-right"></i>
+              <a class=" text-indigo-500 inline-flex items-center">Discover Now
+                <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                     class="w-4 h-4 ml-2" viewBox="0 0 24 24">
+                  <path d="M5 12h14M12 5l7 7-7 7"></path>
+                </svg>
+              </a>
             </div>
           </div>
         </div>
+
 
         <div class="bg-[#FFFFFF] col-span-1 ">
-          <div class="mx-[40px] my-[2rem]">
-            <div class="text-center">
-              <img src="" alt="">
-              icon
+          <div class="mx-[20px] my-[2rem]">
+            <div class="flex justify-center ">
+              <div class="w-[120px] h-[120px] relative">
+                <img
+                    src="		https://wp.dreamitsolution.net/itsolve/wp-content/themes/itsolve/assets/images/service3.png"
+                    alt="">
+                <div class="absolute top-[20%] right-[40%]">
+                  <i class="fas fa-shield-alt text-[#FFFFFF] text-5xl"></i>
+                </div>
+              </div>
+
             </div>
-            <div class="text-[#1e1666] mt-[1rem] text-[24px] font-[700] text-center leading-[31px] ">
+            <div class="text-[#1e1666]  text-[24px] font-[700] text-center leading-[31px] ">
               Interface Design
             </div>
             <div class="text-[#6a7c92] my-[1rem] text-center font-[15px] leading-[28px] ">
@@ -133,18 +151,31 @@
               know.
             </div>
             <div class="text-center cursor-pointer">
-              <span class="text-[#2513d1]">Discover Now</span> <i class="fa-solid text-[#2513d1] fa-arrow-right"></i>
+              <a class=" text-indigo-500 inline-flex items-center">Discover Now
+                <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                     class="w-4 h-4 ml-2" viewBox="0 0 24 24">
+                  <path d="M5 12h14M12 5l7 7-7 7"></path>
+                </svg>
+              </a>
             </div>
           </div>
         </div>
+
 
         <div class="bg-[#FFFFFF] col-span-1 ">
-          <div class="mx-[40px] my-[2rem]">
-            <div class="text-center">
-              <img src="" alt="">
-              icon
+          <div class="mx-[20px] my-[2rem]">
+            <div class="flex justify-center ">
+              <div class="w-[120px] h-[120px] relative">
+                <img
+                    src="			https://wp.dreamitsolution.net/itsolve/wp-content/themes/itsolve/assets/images/service1.png"
+                    alt="">
+                <div class="absolute top-[20%] right-[40%]">
+                  <i class="fas fa-chart-pie text-[#FFFFFF] text-5xl"></i>
+                </div>
+              </div>
+
             </div>
-            <div class="text-[#1e1666] mt-[1rem] text-[24px] font-[700] text-center leading-[31px] ">
+            <div class="text-[#1e1666]  text-[24px] font-[700] text-center leading-[31px] ">
               Interface Design
             </div>
             <div class="text-[#6a7c92] my-[1rem] text-center font-[15px] leading-[28px] ">
@@ -152,18 +183,30 @@
               know.
             </div>
             <div class="text-center cursor-pointer">
-              <span class="text-[#2513d1]">Discover Now</span> <i class="fa-solid text-[#2513d1] fa-arrow-right"></i>
+              <a class=" text-indigo-500 inline-flex items-center">Discover Now
+                <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                     class="w-4 h-4 ml-2" viewBox="0 0 24 24">
+                  <path d="M5 12h14M12 5l7 7-7 7"></path>
+                </svg>
+              </a>
             </div>
           </div>
         </div>
+
 
         <div class="bg-[#FFFFFF] col-span-1 ">
-          <div class="mx-[40px] my-[2rem]">
-            <div class="text-center">
-              <img src="" alt="">
-              icon
+          <div class="mx-[20px] my-[2rem]">
+            <div class="flex justify-center ">
+              <div class="w-[120px] h-[120px] relative">
+                <img src="	https://wp.dreamitsolution.net/itsolve/wp-content/themes/itsolve/assets/images/service5.png"
+                     alt="">
+                <div class="absolute top-[20%] right-[40%]">
+                  <i class="fas fa-comment text-[#FFFFFF] text-5xl"></i>
+                </div>
+              </div>
+
             </div>
-            <div class="text-[#1e1666] mt-[1rem] text-[24px] font-[700] text-center leading-[31px] ">
+            <div class="text-[#1e1666]  text-[24px] font-[700] text-center leading-[31px] ">
               Interface Design
             </div>
             <div class="text-[#6a7c92] my-[1rem] text-center font-[15px] leading-[28px] ">
@@ -171,11 +214,46 @@
               know.
             </div>
             <div class="text-center cursor-pointer">
-              <span class="text-[#2513d1]">Discover Now</span> <i class="fa-solid text-[#2513d1] fa-arrow-right"></i>
+              <a class=" text-indigo-500 inline-flex items-center">Discover Now
+                <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                     class="w-4 h-4 ml-2" viewBox="0 0 24 24">
+                  <path d="M5 12h14M12 5l7 7-7 7"></path>
+                </svg>
+              </a>
             </div>
           </div>
         </div>
 
+
+        <div class="bg-[#FFFFFF] col-span-1 ">
+          <div class="mx-[20px] my-[2rem]">
+            <div class="flex justify-center ">
+              <div class="w-[120px] h-[120px] relative">
+                <img src="	https://wp.dreamitsolution.net/itsolve/wp-content/themes/itsolve/assets/images/service6.png"
+                     alt="">
+                <div class="absolute top-[20%] right-[40%]">
+                  <i class="fas fa-calendar text-[#FFFFFF] text-5xl"></i>
+                </div>
+              </div>
+
+            </div>
+            <div class="text-[#1e1666]  text-[24px] font-[700] text-center leading-[31px] ">
+              Interface Design
+            </div>
+            <div class="text-[#6a7c92] my-[1rem] text-center font-[15px] leading-[28px] ">
+              IT Solve professional business theme for your company, support quality all time, any help please late me
+              know.
+            </div>
+            <div class="text-center cursor-pointer">
+              <a class=" text-indigo-500 inline-flex items-center">Discover Now
+                <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                     class="w-4 h-4 ml-2" viewBox="0 0 24 24">
+                  <path d="M5 12h14M12 5l7 7-7 7"></path>
+                </svg>
+              </a>
+            </div>
+          </div>
+        </div>
 
       </div>
     </div>
@@ -183,20 +261,129 @@
   </div>
 
   <div class="container">
-    <div class="text-center line my-[1rem] text-[#1e1666] text-[38px] leading-[45px]">
-      How IT Work
-    </div>
-    <div class="text-center text-[#6a7c92]    text-[0.938rem] leading-[1.75rem]">
-      <div class="px-[20rem]">
-        Business solution company sit our any how site used the our company any site us it-solve theme is very
-        professional theme business & corporate, finance, advisor, solution, company and all project used, there are
-        all
-        kinds of websites here.
+
+
+    <section class="text-gray-600 body-font">
+      <div class="container px-5  mx-auto">
+        <div class="text-center mb-[3rem]">
+          <div class="text-center line my-[1rem] text-[#1e1666] text-[38px] leading-[45px]">
+            How IT Work
+          </div>
+          <div class="text-center text-[#6a7c92]    text-[0.938rem] leading-[1.75rem]">
+            <div class="px-[20rem]">
+              Business solution company sit our any how site used the our company any site us it-solve theme is very
+              professional theme business & corporate, finance, advisor, solution, company and all project used, there
+              are
+              all
+              kinds of websites here.
+            </div>
+          </div>
+
+          <div class="flex justify-center items-center">
+            <div class="bg-[#2513d1] mt-[1rem]  w-[5%] h-[4px]">
+            </div>
+          </div>
+
+        </div>
+        <div class="flex flex-wrap     md:space-y-0 space-y-6 ">
+          <div class="p-4 md:w-1/3 flex flex-col text-center items-center">
+            <div
+                class="w-20 h-20 inline-flex items-center justify-center rounded-full bg-indigo-100 text-indigo-500 mb-5 flex-shrink-0">
+              <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                   class="w-10 h-10" viewBox="0 0 24 24">
+                <path d="M22 12h-4l-3 9L9 3l-3 9H2"></path>
+              </svg>
+            </div>
+            <div class="flex-grow">
+              <h2 class="text-gray-900 text-lg title-font font-medium mb-3">Shooting Stars</h2>
+              <p class="leading-relaxed text-base">Blue bottle crucifix vinyl post-ironic four dollar toast vegan
+                taxidermy. Gastropub indxgo juice poutine, ramps microdosing banh mi pug VHS try-hard.</p>
+              <a class="mt-3 text-indigo-500 inline-flex items-center">Learn More
+                <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                     class="w-4 h-4 ml-2" viewBox="0 0 24 24">
+                  <path d="M5 12h14M12 5l7 7-7 7"></path>
+                </svg>
+              </a>
+            </div>
+          </div>
+          <div class="p-4 md:w-1/3 flex flex-col text-center items-center">
+            <div
+                class="w-20 h-20 inline-flex items-center justify-center rounded-full bg-indigo-100 text-indigo-500 mb-5 flex-shrink-0">
+              <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                   class="w-10 h-10" viewBox="0 0 24 24">
+                <circle cx="6" cy="6" r="3"></circle>
+                <circle cx="6" cy="18" r="3"></circle>
+                <path d="M20 4L8.12 15.88M14.47 14.48L20 20M8.12 8.12L12 12"></path>
+              </svg>
+            </div>
+            <div class="flex-grow">
+              <h2 class="text-gray-900 text-lg title-font font-medium mb-3">The Catalyzer</h2>
+              <p class="leading-relaxed text-base">Blue bottle crucifix vinyl post-ironic four dollar toast vegan
+                taxidermy. Gastropub indxgo juice poutine, ramps microdosing banh mi pug VHS try-hard.</p>
+              <a class="mt-3 text-indigo-500 inline-flex items-center">Learn More
+                <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                     class="w-4 h-4 ml-2" viewBox="0 0 24 24">
+                  <path d="M5 12h14M12 5l7 7-7 7"></path>
+                </svg>
+              </a>
+            </div>
+          </div>
+          <div class="p-4 md:w-1/3 flex flex-col text-center items-center">
+            <div
+                class="w-20 h-20 inline-flex items-center justify-center rounded-full bg-indigo-100 text-indigo-500 mb-5 flex-shrink-0">
+              <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                   class="w-10 h-10" viewBox="0 0 24 24">
+                <path d="M20 21v-2a4 4 0 00-4-4H8a4 4 0 00-4 4v2"></path>
+                <circle cx="12" cy="7" r="4"></circle>
+              </svg>
+            </div>
+            <div class="flex-grow">
+              <h2 class="text-gray-900 text-lg title-font font-medium mb-3">Neptune</h2>
+              <p class="leading-relaxed text-base">Blue bottle crucifix vinyl post-ironic four dollar toast vegan
+                taxidermy. Gastropub indxgo juice poutine, ramps microdosing banh mi pug VHS try-hard.</p>
+              <a class="mt-3 text-indigo-500 inline-flex items-center">Learn More
+                <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                     class="w-4 h-4 ml-2" viewBox="0 0 24 24">
+                  <path d="M5 12h14M12 5l7 7-7 7"></path>
+                </svg>
+              </a>
+            </div>
+          </div>
+        </div>
       </div>
-    </div>
+    </section>
+
   </div>
 
 
+  <section class="p-6 dark:bg-gray-800 dark:text-gray-100">
+    <div class="container mx-auto grid justify-center grid-cols-2 text-center lg:grid-cols-3">
+      <div class="flex flex-col justify-start m-2 lg:m-6">
+        <p class="text-4xl font-bold leading-none lg:text-6xl text-[#4b5563]">50+</p>
+        <p class="text-sm sm:text-base text-[#4b5563]">Clients</p>
+      </div>
+      <div class="flex flex-col justify-start m-2 lg:m-6">
+        <p class="text-4xl font-bold leading-none lg:text-6xl text-[#4b5563]">89K</p>
+        <p class="text-sm sm:text-base text-[#4b5563]">Followers on social media</p>
+      </div>
+      <div class="flex flex-col justify-start m-2 lg:m-6">
+        <p class="text-4xl font-bold leading-none lg:text-6xl text-[#4b5563]">3</p>
+        <p class="text-sm sm:text-base text-[#4b5563]">Published books</p>
+      </div>
+      <div class="flex flex-col justify-start m-2 lg:m-6">
+        <p class="text-4xl font-bold leading-none lg:text-6xl text-[#4b5563]">8</p>
+        <p class="text-sm sm:text-base text-[#4b5563]">TED talks</p>
+      </div>
+      <div class="flex flex-col justify-start m-2 lg:m-6">
+        <p class="text-4xl font-bold leading-none lg:text-6xl text-[#4b5563]">22</p>
+        <p class="text-sm sm:text-base text-[#4b5563]">Years of experience</p>
+      </div>
+      <div class="flex flex-col justify-start m-2 lg:m-6">
+        <p class="text-4xl font-bold leading-none lg:text-6xl text-[#4b5563]">10+</p>
+        <p class="text-sm sm:text-base text-[#4b5563]">Workshops</p>
+      </div>
+    </div>
+  </section>
 
 </template>
 
